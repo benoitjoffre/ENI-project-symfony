cd0f064c857c43f522f148a11b3785b1deb13cb9
AmelNa59 <amel.nacer@hotmail.fr>
2021-02-25 14:32:04 +0100

    add form

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\ProfilType;
+use App\Entity\User;
+
 
 class UserController extends AbstractController
 {
@@ -15,4 +20,30 @@ class UserController extends AbstractController
     {
         return $this->render('user/login.html.twig');
     }
+
+ /**
+     * @Route("/profil", name="user_profil")
+     */
+    public function form(Profil $profil= null , Request $request, EntityManagerInterface $manager){
+         
+        if(!$profil){
+            $profil= new Profil();
+        }
+
+             $form = $this->createForm(ProfilType::class, $profil);
+
+                $form->handleRequest($request);
+
+                if($form->isSubmitted() && $form->isValid()){
+                    $profil->getId();
+                }
+
+                    $manager->persist($profil);
+
+                    $manager->flush();
+
+                    return $this->redirectToRoute('user_login',['id' => $profil->getId()]);
+                
+    
 }
+}   
---
+++ src/Form/ProfilType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfilType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('pseudo')
+            ->add('prenom')
+            ->add('nom')
+            ->add('tel')
+            ->add('email')
+            ->add('password')
+            ->add('maphoto')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
